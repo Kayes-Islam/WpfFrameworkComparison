ec23c2a0234b07fbceeef75c749eea356b502f94
Kayes-Islam <kamrul000@gmail.com>
2014-10-09 10:47:38 +1030

    Added solution

---
+++ WpfApplication.Prism/Bootstrapper.cs
@@ -0,0 +1,43 @@
+﻿using WpfApplication.Prism.Installers;
+using Microsoft.Practices.Prism.Modularity;
+using PrismContrib.WindsorExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApplication.Prism
+{
+    public class Bootstrapper : WindsorBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return this.Container.Resolve<Shell>();
+        }
+
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+
+            App.Current.MainWindow = (Window)this.Shell;
+            App.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+
+            ModuleCatalog moduleCatalog = (ModuleCatalog)this.ModuleCatalog;
+            //moduleCatalog.AddModule(typeof(HelloWorldModule.HelloWorldModule));
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+
+            this.Container.Install(new Installer());
+        }
+    }
+}
---
+++ WpfApplication.Prism/Installers/Installer.cs
@@ -0,0 +1,23 @@
+﻿using Castle.MicroKernel.Registration;
+using Castle.MicroKernel.SubSystems.Configuration;
+using Castle.Windsor;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication.Prism.Installers
+{
+    public class Installer : IWindsorInstaller
+    {
+        public void Install(IWindsorContainer container, IConfigurationStore store)
+        {
+            container.Register(
+                Component
+                    .For<Shell>()
+                    .LifestyleSingleton()
+            );            
+        }
+    }
+}
