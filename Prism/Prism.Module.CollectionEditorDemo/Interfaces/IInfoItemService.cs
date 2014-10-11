c2e531a0f02bb50f72bc543cabb12a5456a1502a
Kayes-Islam <kamrul000@gmail.com>
2014-10-11 15:13:13 +1030

    added missing files

---
+++ Prism/Prism.Module.CollectionEditorDemo/Interfaces/IInfoItemService.cs
@@ -0,0 +1,14 @@
+﻿using Prism.Module.CollectionEditorDemo.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prism.Module.CollectionEditorDemo.Interfaces
+{
+    public interface IInfoItemService
+    {
+        List<InfoItemViewModel> GetInfoItems();
+    }
+}
